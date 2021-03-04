c06913618504baa185a05e16b6724b92c3afa03f
Tirumala Vasu <tirumalavasu4u@gmail.com>
2021-03-04 15:49:08 +0530

    fhir updated text added

--- src/views/FHIR.vue
+++ src/views/FHIR.vue
@@ -1,60 +1,110 @@
 <template>
   <div class="max-w-3xl mx-auto my-16 m-pad">
     <div class="block flex justify-end">
-      <a href="files/FHIR DOC_PDF_compressed.pdf" download=""><v-btn color="primary" large class="mb-4" download="">Download pdf</v-btn></a>
-    </div>    
-    <h3 class="mb-8">Driving interoperability in healthcare using HL7® FHIR®</h3>
+      <a href="files/Modak FHIR Solution Accelerator March 2021.pdf" download=""
+        ><v-btn color="primary" large class="mb-4" download=""
+          >Download pdf</v-btn
+        ></a
+      >
+    </div>
+    <h3 class="mb-8">
+      Accelerate your journey to meet the HL7 FHIR Interoperability guidelines
+      set by the USA Centers for Medicare and Medicaid Services (CMS).
+    </h3>
     <p class="border-l-4 border-blue-500 pl-4 my-8 italic">
-      Enabling FHIR will help health care providers serve their customers better by improving interoperability. The faster implementation of FHIR requires handling data integration challenges and building use cases with RESTful APIs rapidly.
+      The Centers of Medicare and Medicaid Services (CMS) has published
+      interoperability guidelines that will transform clinical and
+      administrative data exchange among healthcare payers, providers, and
+      patients. CMS specifically requires API’s (application programming
+      interfaces) from
+      <a
+        href="https://www.cms.gov/Regulations-and-Guidance/Guidance/Interoperability/index"
+        target="_blank"
+        >Health Level 7 (HL7) Fast Healthcare Interoperability Resources (FHIR)
+        v4</a
+      >.
     </p>
     <v-img src="img/backgrounds/enabling_fhir.jpg" height="360"></v-img>
     <p>
-      The exchange of information has always been a challenge in healthcare industry. As patients move from one healthcare provider to another, their Electronic Medical Records (EMRs) and other information like insurance should be made discoverable, accessible, and readable to them, doctors, pharma companies, diagnostic centers and other healthcare vendors.
+      The exchange of information has always been a challenge in healthcare
+      industry but has been accomplished through standards-based exchanges such
+      as HL7 Continuity of Care Document (CCD). However, the CCD is limited in
+      the types of data exchanged, and patients who give consent would benefit
+      by sharing a broader set of data with doctors, hospitals, and even pharma
+      companies, if they so choose. The emergence of Fast Healthcare
+      Interoperability Resource (FHIR) standards aims to transform the
+      healthcare industry in terms of secure exchange of patient healthcare data
     </p>
     <p>
-      The emergence of Fast Healthcare Interoperability Resource (FHIR) has transformed the industry in terms of exchange of healthcare data. FHIR data standard enables seamless and on-request information exchange between healthcare applications.
+      The FHIR standard provides a programmatic way to share healthcare
+      information, such as allergies, medication, immunization records,
+      diagnostic reports and patient insurance and claims. The CMS
+      interoperability rule requires the exchange of specific clinical data
+      fields as per the U.S. Core Data for Interoperability (USCDI).
     </p>
     <p>
-      It aims to define the content and structure of the key healthcare information, such as clinical data like allergies, medication and immunization tracking and diagnostic reports and administrative information like patient insurance.
+      However, the implementation of the FHIR standard by the US healthcare
+      industry during the COVID-19 crises puts significant strain on stretched
+      resources, other priorities, and the availability of technical resources
+      to implement the standard.
     </p>
     <p>
-      However, FHIR standard throws an extensive data integration challenge to healthcare firms. For the companies to be FHIR compliant, all the operational and historic exchangeable data residing in various internal data silos within enterprises needs to be leveraged and then converted into the standards defined by FHIR.
+      According to Gartner (a leading IT advisory firm), healthcare companies
+      are struggling with the uphill task of data integration required for
+      enabling FHIR compliant services to meet the guidelines and timelines set
+      by the CMS.
     </p>
     <p>
-      Currently, the companies associated with healthcare are struggling with the uphill task of data integration required for enabling FHIR compliant services for exchanging critical patient-related data. The current approach requires ingestion and transformation of internal data from various data sources into FHIR data model defined in FHIR server.
+      Modak’s extensive experience in the healthcare industry has enabled an
+      alternative approach, one that is more scalable solution and secure and
+      completely removes the need for storing data in FHIR compliant data model.
+      Modak’s proprietary Fireshots software and the use of ‘thin’, reusable
+      templates lifts the burden off IT teams of maintaining the FHIR data model
+      and the associated FHIR server. Modak’s Fireshots™ solution not only
+      addresses the immediate needs to comply with CMS guidelines, but also
+      enables IT to review prioritization of broader use cases where
+      interoperability is key, such as authorization, utilization management,
+      risk adjustment, care coordination and advanced analytics.
+    </p>
+    <h4 class="my-4">Enabling FHIR with Modak Fireshots™</h4>
+    <p>
+      Modak Fireshots™ provides a metadata driven, low-code web services
+      framework for building APIs. Modak Fireshots™ has been shown to develop
+      and deploy complex APIs 5x faster than the traditional approaches. Modak’s
+      metadata driven template approach enables customers to implement FHIR
+      compliant APIs at an accelerated rate. The robust automated data pipelines
+      ingest data quickly from various data stores, validate the data and Modak
+      Fireshots™ converts data into the FHIR format on the fly, eliminating the
+      need to store data in the FHIR data store. The RESTful APIs receive the
+      FHIR compliant response and send it to the external providers.
     </p>
     <p>
-      The approach should also have the mechanism to automatically synchronize incoming FHIR data with operational data in real time. Any changes made to the internal data silos also need to be propagated to the FHIR server. The continuous maintenance and monitoring of FHIR server data is a huge challenge. Further, the data store will grow into another large external data silo over time.
-    </p>
-    <v-img src="img/backgrounds/conventional_fhir_implementation.png" height="360"></v-img>
-    <p>
-      Alternatively, a promising and faster approach is to replace the FHIR data store with thin templates provided by Modak’s Fireshots. The approach eliminates the necessity of storing data in FHIR compliant data model and lifts the burden off enterprise teams to maintain FHIR data model and associate servers.
-    </p>
-    <h4 class="my-4">
-      Enabling FHIR with Modak’s Fireshots
-    </h4>
-    <p>
-      Fireshots is an existing metadata-driven low-code web services framework from Modak. Fireshots has been shown to develop and deploy complex web services 5x faster than the traditional approaches. This metadata-driven template approach by Modak’s Fireshots enables enterprises implement FHIR at an accelerated rate. The robust automated data pipelines ingest data quickly from various data stores, validate the data and Fireshots converts data into FHIR format on the fly, making the task of storing data in FHIR data model unnecessary. The RESTful APIs receive the FHIR compliant response and send it to the external providers.
+      Modak Fireshots™ creates RESTful APIs rapidly with reusable templates. The
+      template-based approach drastically reduces the time in building RESTful
+      APIs by selecting the appropriate template and completing the necessary
+      information.
     </p>
     <p>
-      The web services product creates RESTful APIs rapidly with the help of readymade templates. The template-based approach drastically reduces the time in building RESTful APIs by selecting the appropriate template and filling in with necessary information.
+      The deployment requires minimal template testing, thereby, drastically
+      reducing development time and cost.
     </p>
     <p>
-      The deployment requires minimum template testing. All this drastically reduces the time in the overall development of use cases. While traditional approaches take months to build RESTful APIs, Fireshots achieves the same in days. Fireshots is completely scalable and can easily be deployed on cloud containers for better availability delivered by Kubernetes.
+      Whilst with traditional approaches, using incumbent software tools, the
+      development of FHIR compliant API’s could take months to develop and
+      deploy. Modak Fireshots™ achieves the same in days. Modak Fireshots™ is
+      completely scalable and can easily be deployed on cloud infrastructure via
+      containers and for high availability with Kubernetes
     </p>
-    <v-img src="img/backgrounds/recommended_fhir_implementation.png" height="360"></v-img>
-    <h4 class="mt-8 mb-4">
-      Modak’s approach, a proven success
-    </h4>
-    <p>
-      For example, consider the use case for exchanging health insurance membership information to external healthcare providers for member validation. Such implementation requires building of FHIR RESTful API that takes values like Member ID and DOB and gets matching demographic information, converts output into FHIR format and sends it to external providers. Modak has recently helped develop such FHIR RESTful API for patient search by leveraging Fireshots at a major healthcare firm in under two weeks. The implementation demonstrated the acceleration built into the Fireshots’ web services framework for developing FHIR APIs.
-    </p>
+    <v-img
+      src="img/backgrounds/recommended_fhir_implementation.png"
+      height="360"
+    ></v-img>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FHIR'
+  name: "FHIR",
 };
 </script>
 
