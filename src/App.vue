2910407ef9fef4475df59e84d8778934672b1332
vasusykam <tirumalavasu.sykam@modak.com>
2023-07-12 03:58:47 +0530

    css changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <v-app class="flex flex-col justify-between">
-    <AppNavbar />
-    <router-view class="pt-24 mb-auto"/>
-    <app-footer />
+    <v-progress-circular v-if="isLoading" indeterminate size="70" color="primary" />
+    <v-main v-else>
+      <AppNavbar />
+      <transition>
+        <router-view class="pt-24"/>
+      </transition>
+      <app-footer />
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import { ref, onMounted, onUnmounted } from 'vue';
 import AppNavbar from '@/components/general/AppNavbar.vue';
 import AppFooter from '@/components/general/AppFooter.vue';
 export default {
@@ -15,9 +21,27 @@ export default {
     AppFooter
   },
   setup() {
-    return {
-      
-    };
+    const isLoading = ref(true);
+
+    const onPageLoad = () => {
+      isLoading.value = false;
+    }
+
+    onMounted(() => {
+      window.addEventListener('load', onPageLoad);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('load', onPageLoad);
+    });
+
+    return { isLoading };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.v-main {
+  padding: 0 !important;
+}
+</style>
